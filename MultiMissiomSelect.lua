04ff480b6cf4869bb9ab9394c3a987ec37425630
Alexander <43313549+AlksSAV@users.noreply.github.com>
2022-11-21 19:38:38 +0300

    redesigned script
    The file has been completely redone, all according to the information of the forum.

--- MultiMissiomSelect.lua
+++ MultiMissiomSelect.lua
@@ -1,46 +1,62 @@
 --[[----------------------------------------------------------------------------
 
+ThreeMissions.lua - ArduPilot Lua script
+
 ------------------------------------------------------------------------------]]
-local mission_loaded = false
-local rc_switch = rc:find_channel_for_option(24)  --AUX FUNC sw for mission restart
-local ENABLE_CHANNEL   = 15 -- 15 CH for 6 position sw
-local lastPwm = rc:get_pwm(ENABLE_CHANNEL)
-if not rc_switch then  -- requires the switch to be assigned in order to run script
-  return
+
+local SCRIPT_NAME = 'ThreeMissions'
+
+-------- USER 'CONSTANTS' - change here, if necessary --------
+local SUB_DIR               = '/APM/scripts/' -- that's where to store the Missions - for SD-Card
+local OPTION_SWITCH         = 300          -- the SCR-switch you selected (300..307)
+
+-------- SCRIPT/ARDUPILOT 'CONSTANTS' --------
+local REPEATER_MS         = 500
+
+local WARN                =   4 --MAV_SEVERITY_WARNING
+local INFO                =   6 --MAV_SEVERITY_INFO
+
+local MODE_AUTO           =  10
+local REASON_RC           =   1
+local REASON_GCS          =   2
+local REASON_INITIALIZED  =  26
+local MISSION_COMPLETE    =   2
+
+-------- SCRIPT VARIABLES --------
+local scripting_rc       -- RC-input-channel for OPTION_SWITCH
+local last_pos     = nil -- last position of RC-switch
+
+
+local function send_msg(msg_type, msg)
+  gcs:send_text(msg_type, string.format('%s: %s', SCRIPT_NAME, msg))
 end
-local FREQ = 500
-local last_pwm = 0
 
 local function read_mission(file_name)
 
-   -- Open file try and read header
-  local file = io.open(file_name,"r")
-  local header = file:read('l')
-  if not header then
-    return update, 1000 --could not read, file probably does not exist
-  end
+  -- Open file
+  local file = io.open(file_name)
 
   -- check header
-  assert(string.find(header,'QGC WPL 110') == 1, file_name .. ': incorrect format')
+  assert(string.find(assert(file:read('l'), string.format('%s: %s not existing or empty', SCRIPT_NAME, file_name)), 'QGC WPL 110') == 1, string.format('%s: %s incorrect format', SCRIPT_NAME, file_name))
 
   -- clear any existing mission
-  assert(mission:clear(), 'Could not clear current mission')
+  assert(mission:clear(), string.format('%s: Could not clear current mission', SCRIPT_NAME))
 
   -- read each line and write to mission
   local item = mavlink_mission_item_int_t()
   local index = 0
   while true do
+
     local data = {}
     for i = 1, 12 do
       data[i] = file:read('n')
       if data[i] == nil then
         if i == 1 then
-          gcs:send_text(6, 'loaded mission: ' .. file_name)
-          file:close()
+          send_msg(INFO, string.format('Mission %s with %d items loaded', file_name, index-1))
           return -- got to the end of the file
         else
           mission:clear() -- clear part loaded mission
-          error('failed to read file')
+          error(string.format('%s: failed to read file', SCRIPT_NAME))
         end
       end
     end
@@ -57,51 +73,58 @@ local function read_mission(file_name)
     item:z(data[11])
 
     if not mission:set_item(index,item) then
-      mission:clear() -- clear part loaded mission
-      error(string.format('failed to set mission item %i',index))
+      mission:clear() -- clear partly loaded mission
+      error(string.format('%s failed to set mission item %i', SCRIPT_NAME, index))
     end
     index = index + 1
   end
-  file:close()
+
 end
 
-function update()
-  local somethingChanged = false
-  if not arming:is_armed() and vehicle:get_mode() ~= ROVER_MODE_HOLD  then --if disarmed and no HOLD mode, wait until armed and HOLD
-    mission_loaded = false
-    return update,1000
+local function change_mission_allowed() -- test if loading a new Mission is allowed
+
+local mode = vehicle:get_mode()
+local mode_reason = vehicle:get_control_mode_reason()
+
+  if ((mode ~= MODE_AUTO) 
+   and
+   ((mode_reason == REASON_RC) or (mode_reason == REASON_GCS) or (mode_reason == REASON_INITIALIZED) or (not arming:is_armed())))
+   or
+   (mission:state() == MISSION_COMPLETE)
+  then
+    return true
+  else
+    send_msg(WARN, string.format('Changing Mission not allowed (mode: %d reason: %d)',mode, mode_reason))
+    return false
   end
-  if (pwm ~= lastPwm) then
-    lastPwm = pwm
-    somethingChanged = true
 end
-  if not mission_loaded then -- if first time after arm and switch is valid then try to load based on switch position
-    local read_mission
-    local pwm = RC:get_pwm(15) -- chanel 15 (6 pos switch)
-    if pwm < 983 then
-      read_mission('mission1.waypoints')
-    elseif pwm < 1186 then
-      read_mission('mission2.waypoints')
-    elseif pwm < 1391 then
-      read_mission('mission3.waypoints')
-    elseif pwm < 1596 then
-      read_mission('mission4.waypoints')
-    elseif pwm < 1800 then
-      read_mission('mission5.waypoints')
-    else
-      read_mission('mission6.waypoints')
-     end
-    mission_loaded = true
-    read_mission(read_mission)
-    end
-    if (not somethingChanged) then return update, FREQ 
-    end -- early return
-  return update, FREQ
-end 
 
-return update, FREQ
+local function switch_has_changed(new_sw_pos) -- action when switch has changed
+  if change_mission_allowed() then
+      local file_name = string.format('%s/Mission#%d.waypoints', SUB_DIR, new_sw_pos)
+      send_msg(INFO, string.format('try to read %s ...', file_name))
+      read_mission(file_name)
+      last_pos = new_sw_pos
+  end
+end
 
+function update_switch()
+-- read switch input (0, 1, 2) from designated scripting RCx_OPTION and check if it has changed
+  local sw_pos = scripting_rc:get_aux_switch_pos()
+  if sw_pos ~= last_pos then
+    switch_has_changed(sw_pos)
+  end
+  return update_switch, REPEATER_MS -- reschedules the loop
+end
 
+function init()
+  scripting_rc = rc:find_channel_for_option(OPTION_SWITCH)
+  if scripting_rc then
+    return update_switch()
+  else
+    send_msg(WARN, string.format('RCx_OPTION = %d not found', OPTION_SWITCH))
+    return init, 10000
+  end
+end
 
-
-
+return init()
